1ae00bf5fcaa3229818c57b7be491b0241644cfa
Joonas Javanainen <joonas.javanainen@gmail.com>
2021-10-31 21:55:01 +0200

    Include license in the releases

--- .github/scripts/prepare_release.sh
+++ .github/scripts/prepare_release.sh
@@ -5,6 +5,7 @@ NAME="${1}"
 
 cp -r build "${NAME}"
 find "${NAME}" -type f -not '(' -name '*.sym' -or -name '*.gb' ')' -delete
+cp LICENSE "${NAME}/"
 rm -f "${NAME}".zip "${NAME}".tar.gz "${NAME}".tar.xz
 zip -r "${NAME}".zip "${NAME}"
 tar -czvf "${NAME}".tar.gz "${NAME}"
